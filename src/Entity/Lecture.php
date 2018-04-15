2e7a23e9e80a6e09df067b2b1dc7f19f47b90fdf
Julius Marozas <marozas.julius@gmail.com>
2018-04-15 23:38:44 +0300

    Refactored Group, Lecture, Module and Student entities to use UUID as their Primary Key.

--- src/Entity/Lecture.php
+++ src/Entity/Lecture.php
@@ -13,8 +13,8 @@ class Lecture
 {
     /**
      * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Column(type="guid")
      */
     private $id;
 
