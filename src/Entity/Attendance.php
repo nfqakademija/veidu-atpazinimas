b9c8f6c034f73c313188f58a4ab60b26e3040f74
Julius Marozas <marozas.julius@gmail.com>
2018-04-26 22:08:51 +0300

    Profiler, Debugger

--- src/Entity/Attendance.php
+++ src/Entity/Attendance.php
@@ -11,23 +11,73 @@ class Attendance
 {
     /**
      * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\OneToMany(targetEntity="Student")
+     * @ORM\ManyToOne(targetEntity="Student")
      */
     private $students;
 
     /**
+     * @ORM\Id()
+     * @ORM\ManyToOne(targetEntity="Lecture")
+     */
+    private $lectures;
+
+    /**
+     * @ORM\Id()
      * @ORM\Column(type="boolean")
      */
     private $attended;
 
-    public function getId()
+    /**
+     * @return mixed
+     */
+    public function getStudents()
     {
-        return $this->id;
+        return $this->students;
+    }
+
+    /**
+     * @param mixed $students
+     * @return Attendance
+     */
+    public function setStudents($students)
+    {
+        $this->students = $students;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLectures()
+    {
+        return $this->lectures;
+    }
+
+    /**
+     * @param Lecture $lectures
+     * @return Attendance
+     */
+    public function setLectures($lectures)
+    {
+        $this->lectures = $lectures;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getAttended(): bool
+    {
+        return $this->attended;
+    }
+
+    /**
+     * @param bool $attended
+     * @return Attendance
+     */
+    public function setAttended(bool $attended)
+    {
+        $this->attended = $attended;
+        return $this;
     }
 }
