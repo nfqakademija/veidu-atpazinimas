2f5045a6bed2bc47e26d7fc3f36d31fd505b88a4
Julius Marozas <marozas.julius@gmail.com>
2018-04-28 12:01:04 +0300

    Configured Python Dockerfile

--- python/web_service.py
+++ python/web_service.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import face_recognition
 
@@ -6,7 +7,7 @@ class Service(BaseHTTPRequestHandler):
     def do_POST(self):
         content_len = int(self.headers.getheader('content-length', 0))
         post_body = self.rfile.read(content_len)
-        
+
 
 httpd = HTTPServer(('localhost', 5000), Service)
 httpd.serve_forever()
